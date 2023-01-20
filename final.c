2f0282667a4376df36d2fd8ec53278df8734a93e
Michael Higgins <55243596+Higgs32584@users.noreply.github.com>
2023-01-20 15:21:33 -0500

    Update final.c

--- final.c
+++ final.c
@@ -27,11 +27,37 @@ void Crypt(unsigned char* data,int dataLength,unsigned int initialValue){
         }
 }
 void charToUnsignedChar(char* charArray, unsigned char* unsignedCharArray, int size) {
-for (int i = 0; i < size; i++) {
-        unsignedCharArray[i] = (unsigned char) charArray[i];
-    }
+	for (int i = 0; i < size; i++) {
+        	unsignedCharArray[i] = (unsigned char) charArray[i];
+    	}
+
+	}
+
+struct LinkedList{
+	int32_t a;
+	char *name;
+	struct LinkedList *next;
+};
+typedef struct LinkedList *node;
+
+
+
+
+node createNode(){
+	node n;
+	n = (node) malloc(sizeof(struct LinkedList));
+	n->name = (char *) malloc(sizeof(char)*16);
+	n->next = NULL;
+	return n;
 
 }
+
+char *test_func(){
+	char *c =(char *)malloc(sizeof(char)*16);
+	return c;
+}
+
+
 int main(int argc,char** argv)
 {  
     printf("%s\n",argv[1]);
@@ -39,26 +65,34 @@ int main(int argc,char** argv)
     file_ptr= fopen(argv[1], "rb");
     char *magic = malloc(sizeof(char)*6);
     long pointer;
+    //struct ll b;
+
     fread(magic,sizeof(char),6,file_ptr);
     fread(&pointer,sizeof(int32_t),1,file_ptr);
     fseek(file_ptr,pointer,SEEK_SET);
     
-    int32_t* data = (int32_t*)malloc(sizeof(int32_t) * 8);
+    int32_t* data=(int32_t*)malloc(sizeof(int32_t)*127);
     int32_t k;
-    for(int i=0;i<8; i++){
-	char *c = malloc(sizeof(char)*16);
-        fread(c,sizeof(char),16,file_ptr);
+    int i=0;
+    //int arraySize=sizeof(int32_t);
+    while(k != 0){
+	char *c = test_func();
+	fread(c,sizeof(char),16,file_ptr);
         fread(&k,sizeof(int32_t),1,file_ptr);
+	printf("%d \n",i);
 	data[i] = k;
+	i++;
 	free(c);
     }
-    for(int j=0;j<8;j++){
+    data=realloc(data,i*sizeof(int32_t));
+    i=i-1;
+    for(int j=0;j<i;j++){
 	       	
 	        fseek(file_ptr,data[j],SEEK_SET);
 		
 		int16_t data_size;
     		int32_t data_pointer;
-    		
+    		//printf("%d %d",data_size,data_pointer);
 		fread(&data_size,sizeof(int16_t),1,file_ptr);
     		fread(&data_pointer,sizeof(int32_t),1,file_ptr);
 		fseek(file_ptr,data_pointer,0);
